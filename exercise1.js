47f49296cf0b05b1c3517f30c99ecfa53d11cbb8
Stella Lau <stellau@uw.edu>
2022-10-21 01:37:35 -0700

    Added exercise

--- exercise1.js
+++ exercise1.js
@@ -23,12 +23,23 @@
         { filter: { field: "Origin", equal: "USA" } },
       ],
     };
-    /* your turn:
+
+    /* Your turn:
      * This (https://vega.github.io/editor/data/seattle-weather.csv) is the data set on Seattle's daily weather for 4 years.
      * Can you plot a histogram/bar chart that shows the aggregate count of days of different weather and grouped by the
      * months in the year? (Use #plot3)
-     * For each bar (month), please show the number of days for each type of weather in a different color.
+     * For each bar (month), please show the number of days for each type of weather in a different color
     */
+    const schema3 = {
+        data: { url: "https://vega.github.io/editor/data/seattle-weather.csv"},
+        mark: "bar",
+        encoding: {
+            x: { timeUnit: "month", field: "date", type: "ordinal" },
+            y: { aggregate: "count", field: "*", type: "quantitative" },
+            color: { field: "weather", type: "nominal"}
+        }
+    };
+    vegaEmbed("#plot3", schema3, { actions: false });
     vegaEmbed("#plot", schema, { actions: false });
     vegaEmbed("#plot2", schema2, { actions: false });
   }
